8c2ae15a6926c55df9d03f61d2532976bf6c22b3
Eric Charnesky <eric.charnesky@careevolution.com>
2016-09-14 16:35:48 -0400

    using a second circle

--- Classes/Classes/CircleExample.cpp
+++ Classes/Classes/CircleExample.cpp
@@ -13,6 +13,12 @@ int main()
 	cout << circle1.area() << endl;
 	cout << circle1.circumference() << endl;
 
+	Circle circle2;
+	circle2.setRadius(50);
+	cout << circle2.getRadius() << endl;
+	cout << circle2.area() << endl;
+	cout << circle2.circumference() << endl;
+
 	string input;
 	cin >> input;
 
